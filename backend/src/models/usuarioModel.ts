1446d33dda36263f75472ccce59f66f9f643df60
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2025-11-29 13:32:28 -0300

    No sistema vamos ter o usuario que contem um nome,email,senha,3 tipos dmin bibliotecario aluno, ira ter matriculo e ele pode está ativo ou nao

---
+++ backend/src/models/usuarioModel.ts
@@ -0,0 +1,36 @@
+import { Sequelize, DataTypes } from 'sequelize';
+
+const usuarioModel = (sequelize: Sequelize, dataTypes: typeof DataTypes) => {
+  const Usuario = sequelize.define("usuario", {
+    nome: {
+      type: dataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: dataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    senha: {
+      type: dataTypes.STRING,
+      allowNull: false
+    },
+    tipo: {
+      type: dataTypes.ENUM('admin', 'bibliotecario', 'aluno'),
+      allowNull: false,
+      defaultValue: 'aluno'
+    },
+    matricula: {
+      type: dataTypes.STRING,
+      allowNull: true, // Só para alunos
+      unique: true
+    },
+    ativo: {
+      type: dataTypes.BOOLEAN,
+      defaultValue: true
+    }
+  });
+  return Usuario;
+};
+
+export default usuarioModel;
