845e509b998a58300ea7d1d4088f985b8edfe099
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2025-11-29 13:35:45 -0300

    Identação

--- backend/src/models/LivroModel.ts
+++ backend/src/models/LivroModel.ts
@@ -1,44 +1,44 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
 const livroModel = (sequelize: Sequelize, dataTypes: typeof DataTypes) => {
-  const Livro = sequelize.define("livro", {
-    titulo: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    autor: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    editora: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    anoPublicacao: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    categoria: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    quantidadeTotal: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 1
-    },
-    quantidadeDisponivel: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 1
-    },
-    localizacao: {
-      type: DataTypes.STRING,
-      allowNull: true 
-    }
-  });
-  
-  return Livro;
+    const Livro = sequelize.define("livro", {
+        titulo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        autor: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        editora: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        anoPublicacao: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        categoria: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        quantidadeTotal: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 1
+        },
+        quantidadeDisponivel: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 1
+        },
+        localizacao: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    });
+
+    return Livro;
 };
 
 export default livroModel;
