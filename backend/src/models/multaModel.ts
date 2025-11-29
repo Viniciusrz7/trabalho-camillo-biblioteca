2baf6c47c55ad8902cbd5b6b0bf636173fb1461d
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2025-11-29 13:46:11 -0300

    A mutla vai ter um emprestimo um usuario um valor da multa 3 status pendente paga e cancelada e a datadopagamento

---
+++ backend/src/models/multaModel.ts
@@ -0,0 +1,32 @@
+import { Sequelize, DataTypes } from 'sequelize';
+
+const multaModel = (sequelize: Sequelize, dataTypes: typeof DataTypes) => {
+  const Multa = sequelize.define("multa", {
+    emprestimoId: {
+      type: dataTypes.INTEGER,
+      allowNull: false
+    },
+    usuarioId: {
+      type: dataTypes.INTEGER,
+      allowNull: false
+    },
+    valorMulta: {
+      type: dataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      defaultValue: 0.00
+    },
+    status: {
+      type: dataTypes.ENUM('pendente', 'paga', 'cancelada'),
+      allowNull: false,
+      defaultValue: 'pendente'
+    },
+    dataPagamento: {
+      type: dataTypes.DATE,
+      allowNull: true
+    },
+  });
+  
+  return Multa;
+};
+
+export default multaModel;
