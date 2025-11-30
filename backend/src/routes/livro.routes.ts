dcc0e8305a36e2d1bd4b9bfebf9384a24c3acdd2
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2025-11-29 23:31:56 -0300

    So adicionei os middlewares nas rotas que precisa, para que apenas os usuarios autorizados possa usar as rotas especificadas

--- backend/src/routes/livro.routes.ts
+++ backend/src/routes/livro.routes.ts
@@ -1,15 +1,17 @@
 import { Application } from 'express';
 import * as livroController from '../controllers/livroController';
 import express from 'express';
+import { tokenValidated, verifyAdminOrBibliotecario } from '../middlewares/auth';
 
 const router = express.Router();
-router.post('/', livroController.cadastrarLivro);
-router.get('/', livroController.listarLivros);
-router.get('/:id', livroController.buscarLivroPorId);
-router.put('/:id', livroController.atualizarLivro);
-router.delete('/:id', livroController.deletarLivro);
-router.get('/busca/:query', livroController.buscarLivrosPorTituloOuAutor);
-router.get('/categoria/:categoria', livroController.buscarLivrosPorCategoria);
+
+router.post('/', verifyAdminOrBibliotecario, livroController.cadastrarLivro);
+router.get('/', tokenValidated, livroController.listarLivros);
+router.get('/:id', tokenValidated, livroController.buscarLivroPorId);
+router.put('/:id', verifyAdminOrBibliotecario, livroController.atualizarLivro);
+router.delete('/:id', verifyAdminOrBibliotecario, livroController.deletarLivro);
+router.get('/busca/:query', tokenValidated, livroController.buscarLivrosPorTituloOuAutor);
+router.get('/categoria/:categoria', tokenValidated, livroController.buscarLivrosPorCategoria);
 
 export default (app: Application) => {
   app.use('/livros', router);
