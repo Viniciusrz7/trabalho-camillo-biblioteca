fd41fad54571cacffcf611ebab4b077dc6d1cd24
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2025-12-02 12:41:35 -0300

    Crud para relatorios

---
+++ frontend/Services/relatorioService.ts
@@ -0,0 +1,13 @@
+import { API_URL } from './api';
+import { IEmprestimo } from '@/types';
+import { request } from './utils/utils';
+
+// Livros emprestados por aluno
+export const livrosEmprestadosPorAluno = async (usuarioId: number): Promise<IEmprestimo[]> => {
+  return request(`${API_URL}/relatorios/emprestimos/usuario/${usuarioId}`);
+};
+
+// Livros em atraso
+export const livrosEmAtraso = async (): Promise<IEmprestimo[]> => {
+  return request(`${API_URL}/relatorios/emprestimos/atrasados`);
+};
