fa8e18459de14960f01f4505079c465fd2c6635e
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2025-12-02 12:40:23 -0300

    Colocando uma interface dentro do registrarDevolucao, assim fica melhor o codigo

--- frontend/Services/devolucaoService.ts
+++ frontend/Services/devolucaoService.ts
@@ -1,10 +1,14 @@
 import { API_URL } from './api';
 import { request } from './utils/utils';
 
-// Registrar devolução (admin/bibliotecario)
-export const registrarDevolucao = async (emprestimoId: number) => {
-  return request(`${API_URL}/devolucoes`, {
-    method: 'POST',
-    body: JSON.stringify({ emprestimoId })
-  });
+export const registrarDevolucao = async (emprestimoId: number): Promise<{
+    message: string;
+    diasAtraso: number;
+    multaAplicada: boolean;
+    valorMulta: number;
+}> => {
+    return request(`${API_URL}/devolucoes`, {
+        method: 'POST',
+        body: JSON.stringify({ emprestimoId })
+    });
 };
